ff34f7714fec4ad2802c3941bdd61a6be0d7bbb4
David Beška <david@beska.cz>
2026-03-10 21:23:18 +0100

    Fix build errors

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@gadicc/fetch-mock-cache': false,
-      '@gadicc/fetch-mock-cache/engines/fs': false,
-    };
-    return config;
-  },
+  serverExternalPackages: ['yahoo-finance2'],
 };
 
 module.exports = nextConfig;
